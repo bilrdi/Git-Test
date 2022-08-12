5bd5fb922ae668ce2be6fc90f1813fe46d845ec6
bilrdi <billiumardaeli@gmail.com>
2022-08-12 22:17:14 +0700

    Initial Commit

---
+++ Git Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git Test
+//
+//  Created by Billi Umar Daeli on 12/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git Test/Git_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_TestApp.swift
+//  Git Test
+//
+//  Created by Billi Umar Daeli on 12/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Git_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
